8c931b1fec7afa371d5e5c9ab3c2a3125af414e6
Tobias Grav <tobiagra@stud.ntnu.no>
2022-12-03 01:36:18 +0100

    Made it possible to load GLTF models, and cleaned up code

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
  */
 const express = require('express');
 var fs = require('fs');
+const path = require('path')
 
 //which port number you want to use when starting the localserver.
 const portNumber = 3000;
 
 const app = express();
 
-app.use(express.static("public"))
+app.use(express.static(__dirname + '/public'))
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
 app.get('/', (request, response) => {
 
--- public/scene.js
+++ public/scene.js
@@ -1,4 +1,5 @@
-import * as THREE from "/three/src/Three.js";
+import * as THREE from "./build/three.module.js";
+import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,16 +8,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let object_AK47;
 
-camera.position.z = 5;
+const gltfLoader = new GLTFLoader();
+gltfLoader.load('/AK47/scene.gltf', function(gltf) {
+    object_AK47 = gltf;
+    scene.add(gltf.scene);
+})
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+scene.add(directionalLight);
+
+camera.position.z = 10;
 
 function animate() {
-	cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+	
+    if(object_AK47) {
+        object_AK47.scene.scale.set(1,1,1)
+        object_AK47.scene.rotation.y += 0.01;
+        //object_AK47.scene.rotation.y += 0.01;
+    }
+    
     requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
