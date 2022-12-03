c999f3e39406f48ff573eae070bb2f3b4565c711
Tobias Grav <tobiagra@stud.ntnu.no>
2022-12-03 01:38:15 +0100

    Removed unused code

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
-app.get('/', (request, response) => {
 
-    fs.readFile('./webPage.html', 'utf8', (err, html) => {
-        response.send(html);
-    });
-
-} );
 
 app.listen(portNumber, () => console.log('app available on http://localhost:' + portNumber))
