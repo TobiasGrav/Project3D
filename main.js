2628ce631c629c541b711fbe0585dab3041e7cfb
Tobias Grav <tobiagra@stud.ntnu.no>
2022-12-03 06:47:32 +0100

    Added camera controls

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ var fs = require('fs');
 const path = require('path')
 
 //which port number you want to use when starting the localserver.
-const portNumber = 3000;
+const portNumber = 25565;
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
-
-
 
 app.listen(portNumber, () => console.log('app available on http://localhost:' + portNumber))
--- public/scene.js
+++ public/scene.js
@@ -1,34 +1,56 @@
-import * as THREE from "./build/three.module.js";
+import * as THREE from "three";
 import { GLTFLoader } from './jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from "./OrbitControls.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+//Constructs the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-let object_AK47;
+//Constructs the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('gray');
 
+//Constructs the camera and camera controls
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//Constructs the GLTF loader
 const gltfLoader = new GLTFLoader();
-gltfLoader.load('/AK47/scene.gltf', function(gltf) {
+
+//Objects gets constructed below:
+
+//Lighting objects constructed:
+const ambientLight = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add( ambientLight );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+scene.add(directionalLight);
+
+//GLTF model of the AK47 constructed
+let object_AK47;
+gltfLoader.load('/Models/AK47/scene.gltf', function(gltf) {
     object_AK47 = gltf;
     scene.add(gltf.scene);
 })
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-scene.add(directionalLight);
+//Other logic
 
-camera.position.z = 10;
+//When the window size changes the renderer gets re-adjusted to fit the window
+window.addEventListener('resize', function() {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+camera.position.x = 10;
 
 function animate() {
 	
     if(object_AK47) {
         object_AK47.scene.scale.set(1,1,1)
-        object_AK47.scene.rotation.y += 0.01;
-        //object_AK47.scene.rotation.y += 0.01;
     }
-    
+
     requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
