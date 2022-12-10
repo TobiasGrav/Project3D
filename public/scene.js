a8fc3ca5bf5a8d1bb828c46d74fb9d8b415c3a2c
Tobias Grav <tobiagra@stud.ntnu.no>
2022-12-10 02:10:49 +0100

    Added a heatTick which will make the vertex red by one tick, and a checkForSun function which will check if the vertex is in the precense of the sun.

--- public/scene.js
+++ public/scene.js
@@ -34,11 +34,13 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(directionalLight);
 
 //Defined the objects belonging to the parkPlane mesh
-let parkGeometry, parkMaterial, parkPlane;
+let parkGeometry, parkMaterial, parkPlane, parkLength, parkHeight;
 
 //Function responsible for initilizing the variables defined above
-function createParkPlaneGeometry() {
-    parkGeometry = new THREE.PlaneBufferGeometry(10,15, 100, 150);
+function createParkPlaneGeometry(length, height) {
+    parkLength = length;
+    parkHeight = height;
+    parkGeometry = new THREE.PlaneBufferGeometry(parkLength, parkHeight, 3, 2);
     const count = parkGeometry.attributes.position.count;
     const colors = [];
 
@@ -49,7 +51,7 @@ function createParkPlaneGeometry() {
     }
 
     parkGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-    const parkMaterial = new THREE.MeshBasicMaterial( {wireframe: false, vertexColors: true} );
+    const parkMaterial = new THREE.MeshBasicMaterial( {wireframe: true, vertexColors: true} );
 
     parkPlane = new THREE.Mesh(parkGeometry, parkMaterial);
     parkPlane.rotation.x = -Math.PI/2;
@@ -57,7 +59,7 @@ function createParkPlaneGeometry() {
 };
 
 //initializes the parkPlane
-createParkPlaneGeometry();
+createParkPlaneGeometry(30, 20);
 
 //Function which gets a list of numbers and rearanges them into a list that consists of multiple (x,y,z) values
 function getPoints(geometry) {
@@ -70,6 +72,26 @@ function getPoints(geometry) {
         points.push(point);
     }
     return points;
+}
+
+function tickHeat(vertexIndex) {
+    const heatTickValue = 0.0027778 //2 / (12*60); // this is the value which will tick the rgb value, why 2 / (12*60)? this is the amount of minutes in a day.
+    const vertexColor = parkGeometry.attributes.color;
+    const vertexCurrentColor = [vertexColor.getX(vertexIndex), vertexColor.getY(vertexIndex), vertexColor.getZ(vertexIndex)];
+    
+    if(vertexCurrentColor[2] > 0) {
+        vertexColor.setZ(vertexIndex, vertexCurrentColor[2] - heatTickValue);
+        
+    } else {
+        vertexColor.setY(vertexIndex, vertexCurrentColor[1] - heatTickValue);
+    }
+
+    if(vertexColor.getZ(vertexIndex) < 0) {
+        vertexColor.setZ(vertexIndex, 0);
+    }
+    if(vertexColor.getY(vertexIndex) < 0) {
+        vertexColor.setY(vertexIndex, 0);
+    }
 }
 
 //Red box object constructed
@@ -139,6 +161,24 @@ gltfLoader.load('/Models/OrangeHouse/OrangeHouse.glb', function(gltf){
 
 //Other logic
 
+// gets the points of all the vertices in the world position
+function getWorldVerticesPoints() {
+
+}
+
+function checkForSun(vertexIndex) {
+    const vertexPosition = parkGeometry.attributes.position;
+    const LocalVertexIndexPoint = new THREE.Vector3(vertexPosition.getX(vertexIndex), vertexPosition.getY(vertexIndex), vertexPosition.getZ(vertexIndex));
+    const WorldVertexIndexPoint = LocalVertexIndexPoint.applyQuaternion(parkPlane.quaternion) //applies the quaternion from the world object to the vertex point, so that we get the correct rotation of the vecore
+    const rayVector = new THREE.Vector3(0, Math.sin(sunAngle), -Math.cos(sunAngle));
+
+    const rayCaster = new THREE.Raycaster();
+    rayCaster.set(WorldVertexIndexPoint, rayVector, 0, 10);
+    parkGeometry.attributes.color.setY(vertexIndex, 0);
+    parkGeometry.attributes.color.setZ(vertexIndex, 0);
+    scene.add(new THREE.ArrowHelper(rayVector, WorldVertexIndexPoint, 10, 0xff0000));
+}
+
 //When the window size changes the renderer gets re-adjusted to fit the window
 window.addEventListener('resize', function() {
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -147,7 +187,24 @@ window.addEventListener('resize', function() {
     camera.updateProjectionMatrix();
 });
 
+var sunAngle = 0;
+var sunTick = 0;
+
+//tickHeat(Math.floor(Math.random() * parkGeometry.attributes.position.count));
+
+window.setTimeout(function(){
+    for(;sunTick < 720;) {
+    for(let i = 0; i < parkGeometry.attributes.position.count; i++) {
+        checkForSun(i);
+    }
+    sunAngle += Math.PI/720;
+    sunTick++;
+};
+}, 1000);
+
 function animate() {
+
+    parkGeometry.attributes.color.needsUpdate = true;
 
     /*if(building1) {
         building1.rotation.y += 0.01;
